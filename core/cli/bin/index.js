1780e4252807e1f1aa81f5bef64c4be8484777ff
dingsheng <dingsheng@qdspr.com>
2022-03-03 22:32:06 +0800

    按功能模块拆包

---
+++ core/cli/bin/index.js
@@ -0,0 +1,11 @@
+#! /usr/bin/env node
+
+const importLocal = require('import-local')
+const log = require('npmlog')
+
+if(importLocal(__filename)) {
+    // 本地项目的node_modules中如果存在库则 使用本地库而非全局库
+    log.info('cli', '正在使用 ds-cli 本地版本')
+} else {
+    require('../lib')(process.argv.slice(2))
+}
